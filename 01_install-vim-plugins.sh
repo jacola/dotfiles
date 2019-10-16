c24f8de14e86b9e8e94ccdd9d6224b8169a4ef56
Jacques Lalancette <me@jacques.io>
2019-10-16 22:32:10 +0900

    Updates for react

--- 01_install-vim-plugins.sh
+++ 01_install-vim-plugins.sh
@@ -5,11 +5,13 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 
 git clone https://github.com/mxw/vim-jsx.git ~/.vim/bundle/vim-jsx
+git clone https://github.com/othree/yajs.vim.git
 git clone git://github.com/tpope/vim-commentary.git
 git clone git://github.com/tpope/vim-fugitive.git
 git clone git://github.com/tpope/vim-surround.git
 git clone git://github.com/plasticboy/vim-markdown.git
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone git://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs7
 
 cd ~/.vim/colors
 wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
